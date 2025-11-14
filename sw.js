30b812dd99ea84c072d0d16a93f4e484b2643bed
gunterstruck <224373224+gunterstruck@users.noreply.github.com>
2025-11-14 15:08:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 // REPO_PATH definiert für THiXX-OTH Projekt
 const REPO_PATH = '/THiXX-OTH/';
 // Cache-Version - erhöht nach Bugfix
-const CORE_CACHE_NAME = 'thixx-oth-core-v2';
+const CORE_CACHE_NAME = 'thixx-oth-core-v3';
 const DOC_CACHE_PREFIX = 'thixx-oth-docs';
 
 // Core Assets für Offline-Verfügbarkeit
@@ -249,4 +249,5 @@ self.addEventListener('sync', (event) => {
         );
     }
 });
+
 
